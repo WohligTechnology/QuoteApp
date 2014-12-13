a8685aa707e409b1b96a529d0696c2f9fa15cad9
Tushar <Tushar@Tushars-MBP.mshome.net>
2014-12-13 17:11:42 +0530

    Chakedo re Chakedo..

--- js/quotationsCtrl.js
+++ js/quotationsCtrl.js
@@ -1,17 +1,39 @@
-angular.module('quotationsController', ['quotationsFactory'])
-.controller('QuotationsCtrl', function($scope, $ionicModal, $timeout,quotationsFactory) {
-    
-var ongetsuccess=function (data) {
+angular.module('quotationsController', ['quotationsFactory', 'ionic'])
+    .controller('QuotationsCtrl', function($scope, $ionicModal, $timeout, quotationsFactory) {
+        // Quotation API
+        $scope.chakado = true;
+        $scope.moreDataCanBeLoaded = function() {
+            return $scope.chakado;
+        }
+
+        $scope.quotations = [];
+        var ongetsuccess = function(data) {
+            console.log("THIS IS AAAAAAAA");
             console.log(data);
-            $scope.quotations = data.queryresult;
+            $scope.lastpage = data.lastpage;
+
+            for (var i = 0; i < data.queryresult.length; i++) {
+                $scope.quotations.push(data.queryresult[i]);
+            }
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            if (data.pageno == data.lastpage) {
+                $scope.chakado = false;
+            }
+            firstpage++;
         };
-        var ongeterror=function(data) {
+        var ongeterror = function(data) {
             console.error(data);
         };
-        
-        quotationsFactory.getQuotations(ongetsuccess,ongeterror);
+        var firstpage = 1;
+        quotationsFactory.getQuotations(ongetsuccess, ongeterror, firstpage);
 
-    
-    
-    
-})
+        //Loadmore
+        $scope.loadMore = function() {
+            console.log(firstpage);
+            if (firstpage <= $scope.lastpage) {
+                quotationsFactory.getQuotations(ongetsuccess, ongeterror, firstpage);
+            };
+        };
+
+
+    });
--- js/quotationsFactory.js
+++ js/quotationsFactory.js
@@ -1,10 +1,8 @@
 angular.module('quotationsFactory', [])
-    .factory('quotationsFactory', function ($http) {
+    .factory('quotationsFactory', function($http) {
         var quotations = {};
-        quotations.getQuotations = function (callbacksuccess, callbackerror) {
-            $http.get(ajaxurl+'json/getallquotations', {}).success(callbacksuccess).error(callbackerror);
+        quotations.getQuotations = function(callbacksuccess, callbackerror, pageno) {
+            $http.get(ajaxurl + 'json/getallquotations?pageno=' + pageno, {}).success(callbacksuccess).error(callbackerror);
         };
-        
-        
         return quotations;
     });
