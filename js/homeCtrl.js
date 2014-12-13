5aa1c4cc097d8959594be5803cc9580b6cd22555
Tushar <Tushar@Tushars-MBP.mshome.net>
2014-12-13 17:30:09 +0530

    Chishal

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,5 +1,5 @@
 angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactory'])
-    .controller('HomeCtrl', function ($scope, $ionicPopup, $timeout, $location, homeFactory, vendorFactory, companyFactory) {
+    .controller('HomeCtrl', function($scope, $ionicPopup, $timeout, $location, homeFactory, vendorFactory, companyFactory,$ionicScrollDelegate) {
 
         $scope.elementData = {};
         $scope.elementData.elements = [{
@@ -12,7 +12,7 @@ angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactor
         }, ];
 
         //Adding Elements
-        $scope.addelements = function () {
+        $scope.addelements = function() {
 
             $scope.elementData.elements.push({
                 title: "",
@@ -22,15 +22,18 @@ angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactor
                 price: "",
                 amount: ""
             });
+            
+            $ionicScrollDelegate.scrollBottom(true);
+
         };
         //Removing Elements        
-        $scope.removeElements = function (element) {
+        $scope.removeElements = function(element) {
             var indexwill = $scope.elementData.elements.indexOf(element);
             $scope.elementData.elements.splice(indexwill, 1);
 
         };
 
-        $scope.changedescstatus = function (element) {
+        $scope.changedescstatus = function(element) {
             if (element.showdescription) {
                 element.showdescription = false;
             } else {
@@ -39,67 +42,67 @@ angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactor
         };
 
         //Adding New Company
-        $scope.showCompanyAddPopup = function () {
+        $scope.showCompanyAddPopup = function() {
             $location.path("/app/addCompany");
         }
         //Adding New Vendor/Client
-        $scope.showVendorAddPopup = function () {
+        $scope.showVendorAddPopup = function() {
             $location.path("/app/addVendor");
         }
 
         //Getting ALl Companies
-        var ongetsuccess = function (data) {
+        var ongetsuccess = function(data) {
             console.log(data);
             $scope.companies = data.queryresult;
         };
 
-        var ongeterror = function (data) {
+        var ongeterror = function(data) {
             console.error(data);
         };
 
         companyFactory.getCompanies(ongetsuccess, ongeterror);
 
         //Get All Vendors        
-        var ongetsuccess = function (data) {
+        var ongetsuccess = function(data) {
             console.log(data);
             $scope.vendors = data.queryresult;
         };
-        var ongeterror = function (data) {
+        var ongeterror = function(data) {
             console.error(data);
         };
 
         vendorFactory.getVendors(ongetsuccess, ongeterror);
-//        //Get Quotation Id
-//        var ongetsuccess = function (data) {
-//            console.log(data);
-//            $scope.elementData.quotationId = data;
-//        };
-//        var ongeterror = function (data) {
-//            console.error(data);
-//        };
-//
-//        homeFactory.getQuotationId(ongetsuccess, ongeterror);
+        //        //Get Quotation Id
+        //        var ongetsuccess = function (data) {
+        //            console.log(data);
+        //            $scope.elementData.quotationId = data;
+        //        };
+        //        var ongeterror = function (data) {
+        //            console.error(data);
+        //        };
+        //
+        //        homeFactory.getQuotationId(ongetsuccess, ongeterror);
         //Temporary Calculations
 
 
 
 
-        $scope.elementData.taxRate=0;
+        $scope.elementData.taxRate = 0;
 
-        $scope.showPreview = function () {
+        $scope.showPreview = function() {
             console.log($scope.elementData);
-            
-            var onSendSucess = function(data){
+
+            var onSendSucess = function(data) {
                 console.log(data);
-                
+
             }
-            var OnSendError = function(data){
+            var OnSendError = function(data) {
                 console.log(data);
             }
-            homeFactory.sendQuote($scope.elementData,onSendSucess,OnSendError)
-            
-            
-            
+            homeFactory.sendQuote($scope.elementData, onSendSucess, OnSendError)
+
+
+
         }
         //        $scope.i = 0;
         //        $scope.$watch('elementData.elements', function (newValue, oldValue) {
@@ -111,7 +114,7 @@ angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactor
         //        });
         $scope.element = $scope.elementData.elements;
         $scope.$watch(
-            function (element) {
+            function(element) {
                 $scope.elementData.totalQuantity = 0;
                 $scope.elementData.totalAmount = 0;
                 for (i = 0; i < $scope.elementData.elements.length; i++) {
@@ -119,14 +122,14 @@ angular.module('homeController', ['homeFactory', 'vendorFactory', 'companyFactor
                     var num = parseInt($scope.elementData.elements[i].quantity);
                     if (!isNaN(num)) {
                         $scope.elementData.totalQuantity += parseInt($scope.elementData.elements[i].quantity);
-                        $scope.elementData.totalAmount += parseFloat($scope.elementData.elements[i].price*$scope.elementData.elements[i].quantity);
+                        $scope.elementData.totalAmount += parseFloat($scope.elementData.elements[i].price * $scope.elementData.elements[i].quantity);
                     }
-                    
+
 
                 }
                 //console.log($scope.elementData.totalQuantity);
             },
-            function () {}
+            function() {}
         );
 
 
