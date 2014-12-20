68b4d84c0085719b1b9f44d5c1115baba2cc37b9
422vishal <422vishal@gmail.com>
2014-12-20 18:01:26 +0530

    Client ID

--- js/loginCtrl.js
+++ js/loginCtrl.js
@@ -2,7 +2,7 @@ angular.module('loginController', ['ionic', 'ngCordova'])
 .controller('LoginCtrl', function($scope, $ionicModal, $timeout,$cordovaOauth) {
     
     $scope.googleLogin = function() {
-        $cordovaOauth.google("CLIENT_ID_HERE", ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
+        $cordovaOauth.google(499535922670, ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
             console.log(JSON.stringify(result));
         }, function(error) {
             console.log(error);
