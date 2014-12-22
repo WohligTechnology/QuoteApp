7849845c790a93313c3cbc3cf23e0de027529fbe
wohlig <chintan@wohlig.com>
2014-12-22 20:51:02 +0530

    wohlig Link
    Wohlig Link

--- js/loginCtrl.js
+++ js/loginCtrl.js
@@ -2,6 +2,6 @@ angular.module('loginController', ['ionic', 'ngCordova'])
     .controller('LoginCtrl', function ($scope, $ionicModal, $timeout, $cordovaOauth) {
 
         $scope.twitterlogin = function () {
-            var ref = window.open('http://localhost/QuotationApp/admin/index.php/hauth/login/Twitter', '_blank', 'location=yes');
+            var ref = window.open('http://wohlig.biz/quotation/index.php/hauth/login/Twitter', '_blank', 'location=yes');
         };
     });
